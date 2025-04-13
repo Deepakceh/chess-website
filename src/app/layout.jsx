4c4761b8781c2ce77388aaac4bae899426ea458c
deepaksapphiretech <deepak.kumar@sapphiretech.co.in>
2025-04-13 23:01:12 +0530

    add whatsapp functionality

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Head from 'next/head';
 import { Poppins } from 'next/font/google';
-
+import WhatsAppButton from '../components/WhatsAppButton'
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
@@ -50,6 +50,7 @@ export default function RootLayout({ children }) {
       <body className="bg-white text-black antialiased">
         <Navbar />
         <main>{children}</main>
+        <WhatsAppButton />
         <Footer />
       </body>
     </html>
